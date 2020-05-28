5f263f4fa26876a9f764165314899d47a4f8b68b
InJun Choi <000xor1@gmail.com>
2020-05-28 20:21:38 +0900

    ENH:

--- estimate/urls.py
+++ estimate/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload', views.upload, name='upload'),
     path('report/<id>', views.report, name='report'),
+    path('api/report/<id>', views.get_report, name='report'),
+    path('api/image/<dir_id>/<sub_dir_id>/<file_id>',
+         views.load_image, name='load_image'),
 ]
--- estimate/views.py
+++ estimate/views.py
@@ -69,18 +69,52 @@ def upload(request):
         return JsonResponse({'status': 'fail', 'msg': "업로드 된 파일이 없습니다."})
 
 
-def report(request, id):
-
+@csrf_exempt
+def get_report(request, id):
     dir_name = id
 
+    # 비디오 파일 이름
     try:
-        file_name = os.listdir(dir_name+"/video")[0]
+        video_name = os.listdir(dir_name+"/video")[0]
     except:
-        file_name = "Not found file"
+        video_name = "Not found file"
 
-    return render(request, 'estimate/report.html', {'dir_name': dir_name, 'file_name': file_name})
+    # 자동차 이미지
+    try:
+        vehicle_frames = [dir_name+'/vehicle_frames/' +
+                          filename for filename in os.listdir(dir_name+"/vehicle_frames") if filename != '.DS_Store']
+    except:
+        vehicle_frames = []
+
+    # 번호판 이미지
+    try:
+        lp_frames = [dir_name+'/lp_frames/' +
+                     filename for filename in os.listdir(dir_name+"/lp_frames") if filename != '.DS_Store']
+
+    except:
+        lp_frames = []
+
+    return JsonResponse({'status': 'success', 'result': {'report': {'dir_name': dir_name, 'video_name': video_name, 'images': {'vehicle_frames': vehicle_frames, 'lp_frames': lp_frames}}}})
+
+
+def load_image(request, dir_id, sub_dir_id, file_id):
+    file_path = os.path.join(dir_id, sub_dir_id, file_id)
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(
+                fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + \
+                os.path.basename(file_path)
+            return response
+
+    # print(dir_id, sub_dir_id, file_id)
+    # return JsonResponse({'status': 'success'})
+
+
+def report(request, id):
+    return render(request, 'estimate/report.html', {"id": id})
 
 
 def index(request):
-    msg = 'My Message'
-    return render(request, 'estimate/index.html', {'message': msg})
+    return render(request, 'estimate/index.html', {})
